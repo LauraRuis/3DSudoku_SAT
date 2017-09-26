4502c8379b1530f33fe29a651f9b7e09ffa33743
LauraRuis <lauraruis@live.nl>
2017-09-26 00:59:08 -0700

    jsontofiles writes empty sudokus

--- jsontofiles.py
+++ jsontofiles.py
@@ -1,29 +1,37 @@
 import os
 import json
 
-json_data = open ('sudoku_data.json').read()
+json_data = open ('data.json').read()
 sudokus = json.loads(json_data)
 currfol = os.getcwd()
 
-three_d = False
+three_d = True
 
+empty_sud = "-1 -1 -1 -1 -1 -1 -1 -1 -1\n-1 -1 -1 -1 -1 -1 -1 -1 -1\n-1 -1 -1 -1 -1 -1 -1 -1 -1\n-1 -1 -1 -1 -1 -1 -1 -1 -1\n-1 -1 -1 -1 -1 -1 -1 -1 -1\n-1 -1 -1 -1 -1 -1 -1 -1 -1\n-1 -1 -1 -1 -1 -1 -1 -1 -1\n-1 -1 -1 -1 -1 -1 -1 -1 -1\n-1 -1 -1 -1 -1 -1 -1 -1 -1\n"
 if three_d:
 
-    for j in range(5000): # write first 10 sudokus
+    for j in range(5354): # write first 10 sudokus
         fn = "sudokus/sud%d.txt" %j
         filename = os.path.join(currfol,fn)
         with open(filename, "w") as f:
             f.write('order 9 3 \n')
             for i in range(9):
-                f.write(sudokus[str(j)][str(i)])
+		if not sudokus[str(j)][str(i)]:
+			f.write(empty_sud)
+		else:
+			f.write(sudokus[str(j)][str(i)])
 
 else:
 
-    for j in range(5000): # write first 10 sudokus
+    for j in range(5354): # write first 10 sudokus
         for i in range(9):
             fn = "sudokus2d/sud%d-%d.txt" % (j, i)
             filename = os.path.join(currfol, fn)
             with open(filename, "w") as f:
                 f.write('order 9 3 \n')
-                f.write(sudokus[str(j)][str(i)])
-                f.close()
+		if not sudokus[str(j)][str(i)]:
+			f.write(empty_sud)
+		else:
+
+		        f.write(sudokus[str(j)][str(i)])
+		        f.close()
